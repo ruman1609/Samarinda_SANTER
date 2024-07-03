c2b6f55ba15ff25fbe8ffa25d2d0a19ad2ede2f2
Rudy Rachman <rudyrachman16@gmail.com>
2024-07-03 15:55:42 +0800

    Selesai jetpack compose, tinggal test.

--- app/src/main/java/com/rudyrachman16/samarindasanter/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/rudyrachman16/samarindasanter/ui/dashboard/DashboardActivity.kt
@@ -3,7 +3,6 @@ package com.rudyrachman16.samarindasanter.ui.dashboard
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,15 +10,12 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.compose.SamarindaSanterTheme
 import com.rudyrachman16.samarindasanter.core.model.News
-import com.rudyrachman16.samarindasanter.databinding.ActivityDashboardBinding
 import com.rudyrachman16.samarindasanter.ui.dashboard.route.AssertNewsParcelable
 import com.rudyrachman16.samarindasanter.ui.dashboard.route.DashboardRoute
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DashboardActivity : AppCompatActivity() {
-    private lateinit var bind: ActivityDashboardBinding
-    private val viewModel: DashboardViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
