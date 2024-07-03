96091606afbf6c2a00ffb008ab7f78389871dead
Rudy Rachman <rudyrachman16@gmail.com>
2024-07-03 15:57:25 +0800

    Selesai jetpack compose, tinggal test.

--- app/src/main/java/com/rudyrachman16/samarindasanter/utils/ViewUtils.kt
+++ app/src/main/java/com/rudyrachman16/samarindasanter/utils/ViewUtils.kt
@@ -2,27 +2,16 @@ package com.rudyrachman16.samarindasanter.utils
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Typeface
 import android.text.Html
-import android.text.style.ForegroundColorSpan
-import android.text.style.StyleSpan
-import android.text.style.UnderlineSpan
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import com.rudyrachman16.samarindasanter.core.Status
 import com.rudyrachman16.samarindasanter.databinding.DialogLoadingBinding
 
 object ViewUtils {
     private var loadingDialog: AlertDialog? = null
-    fun Activity.showLoadingDialog(isLoading: Boolean) {
+    private fun Activity.showLoadingDialog(isLoading: Boolean) {
         if (isLoading) {
             if (loadingDialog == null || loadingDialog?.isShowing != true) {
                 loadingDialog = AlertDialog.Builder(this)
@@ -42,50 +31,6 @@ object ViewUtils {
 
     fun TextView.fromHTML(htmlString: String) {
         text = Html.fromHtml(htmlString, Html.FROM_HTML_MODE_COMPACT)
-    }
-
-    fun String.fromHTML(htmlString: String): AnnotatedString {
-        val spanned = Html.fromHtml(htmlString, Html.FROM_HTML_MODE_COMPACT),
-        return buildAnnotatedString {
-            spanned.getSpans(0, spanned.length, Any::class.java).forEach { span ->
-                val start = spanned.getSpanStart(span)
-                val end = spanned.getSpanEnd(span)
-                when (span) {
-                    is StyleSpan -> when (span.style) {
-                        Typeface.BOLD -> addStyle(
-                            SpanStyle(fontWeight = FontWeight.Bold),
-                            start,
-                            end
-                        )
-
-                        Typeface.ITALIC -> addStyle(
-                            SpanStyle(fontStyle = FontStyle.Italic),
-                            start,
-                            end
-                        )
-
-                        Typeface.BOLD_ITALIC -> addStyle(
-                            SpanStyle(
-                                fontWeight = FontWeight.Bold,
-                                fontStyle = FontStyle.Italic
-                            ), start, end
-                        )
-                    }
-
-                    is UnderlineSpan -> addStyle(
-                        SpanStyle(textDecoration = TextDecoration.Underline),
-                        start,
-                        end
-                    )
-
-                    is ForegroundColorSpan -> addStyle(
-                        SpanStyle(color = Color(span.foregroundColor)),
-                        start,
-                        end
-                    )
-                }
-            }
-        }
     }
 
     fun <T> Context.viewModelStatusConsume(
